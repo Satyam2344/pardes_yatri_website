56e5151adf3e3a47d469433cfe2430cba8e4f158
Satyam Gupta <satgup6902@gmail.com>
2025-05-01 17:41:26 +0530

    Update Commonbg.jsx

--- src/components/Commonbg.jsx
+++ src/components/Commonbg.jsx
@@ -8,7 +8,7 @@ export default function Commonbg({ imageUrl }) {
       <div
         className="w-full h-72 mt-12 sm:h-96 md:h-[600px] lg:h-[500px] xl:h-[400px] bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url(${baseUrl}/${imageUrl})`,
+          backgroundImage: `url(${baseUrl}${imageUrl})`,
         }}
       ></div>
     </section>
